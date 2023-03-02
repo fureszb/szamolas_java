f001fcd53906d2aa086e03b704ca76b6a0be539d
fureszb <fureszb@gmail.com>
2023-03-02 16:28:28 +0100

    bekeres

--- SzamolosFeladatok/src/ciklusok/Szamok.java
+++ SzamolosFeladatok/src/ciklusok/Szamok.java
@@ -1,14 +1,35 @@
 package ciklusok;
 
+import java.util.Scanner;
+
 public class Szamok {
 
     public static void main(String[] args) {
-        for (int k = 2; k <= 4; k++) {
-            for (int i = 2; i <= 4; i++) {
-                for (int j = 2; j <= 4; j++) {
-                    System.out.printf("%d%d%d\n",k, i, j);
+        Scanner bekeres = new Scanner(System.in);
+        System.out.print("Töbször akarunk egy számot felhasználni [igen | nem]: ");
+        String valasz = bekeres.next();
+        System.out.println(valasz);
+        if (valasz.equals("igen")) {
+            for (int k = 2; k <= 4; k++) {
+                for (int i = 2; i <= 4; i++) {
+                    for (int j = 2; j <= 4; j++) {
+                        System.out.printf("%d%d%d\n", k, i, j);
+                    }
                 }
             }
+        } else if (valasz.equals("nem")) {
+            for (int k = 2; k <= 4; k++) {
+                for (int i = 2; i <= 4; i++) {
+                    for (int j = 2; j <= 4; j++) {
+                        if ((i != j) && (k != i)) {
+                            System.out.printf("%d%d%d\n", k, i, j);
+                        }
+
+                    }
+                }
+            }
+        } else {
+            System.out.println("Nincs ilyen lehetőség!");
         }
 
     }
