0a4a2c9b8657c5cf8dcbb5e7de29de78cb37c1fc
fureszb <fureszb@gmail.com>
2023-03-02 16:02:57 +0100

    ciklusok

--- SzamolosFeladatok/src/ciklusok/Szamok.java
+++ SzamolosFeladatok/src/ciklusok/Szamok.java
@@ -1,16 +1,16 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package ciklusok;
 
-/**
- *
- * @author fures
- */
 public class Szamok {
+
     public static void main(String[] args) {
-    
+        for (int k = 2; k <= 4; k++) {
+            for (int i = 2; i <= 4; i++) {
+                for (int j = 2; j <= 4; j++) {
+                    System.out.printf("%d%d%d\n",k, i, j);
+                }
+            }
+        }
+
     }
-    
+
 }
